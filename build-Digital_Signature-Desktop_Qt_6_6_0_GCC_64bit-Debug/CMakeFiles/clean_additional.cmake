f56627e0133359d264a6773c6cbe9dd17430c085
fgecek20 <fgecek20@student.foi.hr>
2023-11-18 18:52:01 +0100

    Project initialisation.

---
+++ build-Digital_Signature-Desktop_Qt_6_6_0_GCC_64bit-Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles/Digital_Signature_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Digital_Signature_autogen.dir/ParseCache.txt"
+  "Digital_Signature_autogen"
+  )
+endif()
